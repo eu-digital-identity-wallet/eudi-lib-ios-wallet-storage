d8583cb27f6b780c1e57ffeba8071f0ddacdc431
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-11-22 01:43:28 +0200

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -13,12 +13,17 @@ let package = Package(
             name: "WalletStorage",
             targets: ["WalletStorage"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WalletStorage", dependencies: []),
+            name: "WalletStorage", 
+            dependencies: [
+                .product(name: "Logging", package: "swift-log")
+                ]),
         .testTarget(
             name: "WalletStorageTests",
             dependencies: ["WalletStorage"]),
