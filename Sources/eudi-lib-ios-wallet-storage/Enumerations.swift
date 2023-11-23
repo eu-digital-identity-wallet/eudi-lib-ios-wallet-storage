17bec9f9a0310de7efd6e5893898efb158cf4a1a
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-11-23 14:39:02 +0200

    update license and comments in Enumerations file

--- Sources/eudi-lib-ios-wallet-storage/Enumerations.swift
+++ Sources/eudi-lib-ios-wallet-storage/Enumerations.swift
@@ -1,31 +1,42 @@
 /*
-* Copyright (c) 2023 European Commission
-*
-* Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the European
-* Commission - subsequent versions of the EUPL (the "Licence"); You may not use this work
-* except in compliance with the Licence.
-*
-* You may obtain a copy of the Licence at:
-* https://joinup.ec.europa.eu/software/page/eupl
-*
-* Unless required by applicable law or agreed to in writing, software distributed under
-* the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
-* ANY KIND, either express or implied. See the Licence for the specific language
-* governing permissions and limitations under the Licence.
+Copyright (c) 2023 European Commission
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 import Foundation
 
+/// type of data to save in storage
 public enum SavedKeyChainDataType{
 	case doc
 	case key
 }
+
+/// Format of document data
+/// ``cbor``: DeviceResponse cbor encoded
+/// ``sjwt``: sd-jwt ** not yet supported **
+/// ``signupResponseJson``: DeviceResponse and PrivateKey json serialized
 public enum DocDataType: String  {
 	case cbor = "cbor"
 	case sjwt = "sjwt"
 	case signupResponseJson = "srjs"
 }
 
+/// Format of private key
+/// ``derEncodedP256``: DER encoded
+/// ``pemStringDataP256`` PEM string encoded as utf8
+/// ``x963EncodedP256``: ANSI x9.63 representation (default)
+/// ``secureEnclaveP256``: data representation for the secure enclave
 public enum PrivateKeyType: String {
 	case derEncodedP256 = "dep2"
 	case pemStringDataP256 = "pep2"
