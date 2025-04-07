5cee86c5096b26cf4b470c00ca4e622204e3d203
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-04-07 09:31:19 +0300

    Update supported platform versions

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WalletStorage",
     defaultLocalization: "en",
-	platforms: [.macOS(.v12), .iOS(.v15), .watchOS(.v9)],
+	platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library( 
