455a7e1e99e96a937d73301ddb3bda8a15847c04
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-05-30 15:31:26 +0300

    Update eudi-lib-ios-iso18013-data-model to version 0.7.0 in Package.resolved and Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", from: "0.6.1"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", from: "0.7.0"),
 		],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
