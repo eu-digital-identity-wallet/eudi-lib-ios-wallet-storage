b8d3c7829b8328ee629ad21021b08af70da94b1d
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-10-06 21:45:35 +0300

    Update eudi-lib-ios-iso18013-data-model to version 0.8.0 and adjust minimum macOS platform version to 14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WalletStorage",
     defaultLocalization: "en",
-	platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v10)],
+	platforms: [.macOS(.v14), .iOS(.v16), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library( 
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", from: "0.7.1"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", from: "0.8.0"),
 		],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
